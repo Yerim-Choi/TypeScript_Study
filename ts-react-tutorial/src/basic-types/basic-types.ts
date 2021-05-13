c84f8285068a54cd9d710cd7b0c1058caa71d906
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-05-13 23:27:06 +0900

    #TypeScript Study

---
+++ ts-react-tutorial/src/basic-types/basic-types.ts
@@ -0,0 +1,25 @@
+// 타입스크립트로 변수나 함수와 같은 자바스크립트 코드에 타입을 정의할 수 있습니다.
+// 타입스크립트의 기본 타입에는 크게 다음 12가지가 있습니다.
+
+// Boolean
+// Number
+// String
+// Object
+// Array
+// Tuple
+// Enum
+// Any
+// Void
+// Null
+// Undefineds
+// Never
+
+let str: string = 'hi';
+let num: number = 10;
+let isLoggedIn: boolean = false;
+
+let arr: number[] = [1,2,3];
+let arr2: Array<number> = [1,2,3];
+
+
+let arr3: [string, number] = ['hi', 10];
